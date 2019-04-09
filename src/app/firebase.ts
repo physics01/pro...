5e8f08896c38dcc435c4d147bab07240690f60c6
physics01 <39649347+physics01@users.noreply.github.com>
2019-04-09 21:05:55 +0700

    Add files via upload

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyA8q-ic7223lonEbvpLDKeezb94AkA9NLw",
+    authDomain: "physics-c3eb1.firebaseapp.com",
+    databaseURL: "https://physics-c3eb1.firebaseio.com",
+    projectId: "physics-c3eb1",
+    storageBucket: "physics-c3eb1.appspot.com",
+    messagingSenderId: "958547420456"
+}
+
+export default config
